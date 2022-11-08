e3ca2a9d052341bca1c220a89384460abd309957
ByeongSooLee-KR <djaxornwkd12@gmail.com>
2022-11-09 00:41:29 +0900

    :wrench: Update: Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,34 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#ffba00',
+        blue: '#0097f3',
+        grey01: '#707070',
+        grey02: '#b2b2b2',
+      },
+      animation: {
+        wiggle: 'wiggle 2s ease-in-out infinite',
+        bounce: 'bounce 1.5s infinite',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-2deg)' },
+          '50%': { transform: 'rotate(2deg)' },
+        },
+        bounce: {
+          '0%, 100%': {
+            transform: 'translateY(-10%)',
+            'animation-timing-function': 'cubic-bezier(0.8, 0, 1, 1)',
+          },
+          '50%': {
+            transform: 'translateY(0)',
+            'animation-timing-function': 'cubic-bezier(0, 0, 0.2, 1)',
+          },
+        },
+      },
+    },
   },
   plugins: [],
-};
+}
