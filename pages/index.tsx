bf4ef6925905d269e47b5ca3b00f30f9e73d291f
ByeongSooLee-KR <djaxornwkd12@gmail.com>
2022-11-16 00:31:36 +0900

    :lipstick: Update: 랜딩페이지 디자인 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,36 @@
-import { useRouter } from "next/router"
+import { useRouter } from 'next/router'
 
 export default function Home() {
-const router = useRouter();
+  const router = useRouter()
 
-return (
-  <div className="h-screen">
-    <div className="mx-auto py-20 px-10 w-96 h-full flex flex-col justify-between">
-      <div>
-        <div className="text-2xl mb-3 text-grey02 ml-9">2022</div>
-        <div className="text-2xl mb-3 flex ml-9">
-          <div className="mr-1 underline underline-offset-4 text-blue font-medium">개발팀</div> 
-          <div className="text-grey02">님의</div>
+  return (
+    <div className="h-screen bg-[url('/images/bg-image.jpeg')]">
+      <div className="flex flex-col items-center justify-around h-full px-10 py-20 mx-auto">
+        <div>
+          <div className="mb-3 text-2xl text-white">2022</div>
+          {/* <div className="flex mb-3 text-2xl ml-9">
+            <div className="mr-1 font-medium text-white underline underline-offset-4">개발팀</div>
+            <div className="text-white">님의</div>
+          </div> */}
+          <div className="flex mb-3 font-bold text-white text-7xl ju">
+            <div className="text-gray-300">술</div>
+            <div className="text-primary">담</div>
+            <div className="text-blue-300">화</div>
+          </div>
+          <div className="mt-5 text-4xl font-medium text-center text-white">마셔보고서</div>
         </div>
-        <div className="text-8xl mb-3 text-primary font-bold text-center">술담화</div>
-        <div className="text-5xl text-primary font-medium text-center">마셔보고서</div>
-      </div>
-      <div className="flex justify-center mb-20 animate-pulse">
-        <img src="/images/sooldamhwa-logo.png" width={150} height={150}/>
-      </div>
-      <div className="flex justify-end">
-        <button className="w-24 h-24 rounded-full bg-primary text-white text-2xl animate-bounce" onClick={() => router.push('/loading')}>시작</button>
+        <div className="flex justify-center mb-10 animate-pulse">
+          <img src="/images/sooldamhwa-logo.png" width={150} height={150} />
+        </div>
+        <div className="flex justify-end w-full">
+          <button
+            className="w-24 h-24 text-2xl text-white rounded-full bg-primary animate-bounce"
+            onClick={() => router.push('/loading')}
+          >
+            시작
+          </button>
+        </div>
       </div>
     </div>
-  </div>
   )
 }
