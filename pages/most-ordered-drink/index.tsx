cb020e175fc61405586fac1146b22c7aa897942a
hubiwibe <hubiwibe@gmail.com>
2022-11-22 22:36:40 +0900

    🌱 Add: 가장 많이 주문한 술 api 연동

--- pages/most-ordered-drink/index.tsx
+++ pages/most-ordered-drink/index.tsx
@@ -2,12 +2,32 @@ import React from 'react';
 import AutoOpenImage from '../../components/AutoOpenImage';
 import Layout from '../../components/layout';
 
-function index() {
+function index({ imgUrl }: { imgUrl: string }) {
   return (
     <Layout>
-      <AutoOpenImage imgUrl="https://d38cxpfv0ljg7q.cloudfront.net/admin_contents/detail/5YHz-1651137039043-1.jpg" />
+      <AutoOpenImage imgUrl={imgUrl} />
     </Layout>
   );
 }
 
 export default index;
+
+export const getServerSideProps = async () => {
+  const response = await fetch(
+    'http://api-side.sooldamhwa.com/masheo/most-ordered-sool?userId=120'
+  );
+  const {
+    data: {
+      MostOrderedSool: { thumbnail },
+    },
+  } = await response.json();
+
+  return {
+    props: {
+      imgUrl: thumbnail?.replace(
+        'https://sooldamhwa-static-files.s3.ap-northeast-2.amazonaws.com/admin_contents/origin/',
+        'https://d38cxpfv0ljg7q.cloudfront.net/admin_contents/detail/'
+      ),
+    },
+  };
+};
