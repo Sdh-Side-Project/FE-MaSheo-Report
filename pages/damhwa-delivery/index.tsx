49ddbc3ec2b383ca87e4bcde46484cace8b07288
Namkyung <srirachacho@gmail.com>
2022-12-05 02:29:44 +0900

    :seedling: Add: 담화배송 횟수 페이지 추가

---
+++ pages/damhwa-delivery/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import DamhwaDeliveryContainer from '../../components/DamhwaDelivery/DamhwaDeliveryComponent';
+import Layout from '../../components/layout';
+
+const DamhwaDelivery = () => {
+  return (
+    <Layout
+      user={'개발팀'}
+      firstLine={'올해 이용한'}
+      keyword={'담화배송 횟수'}
+      closingLine={'는?'}
+      footerMessage={`내년에도 담화배송을 잘 부탁해요!`}
+    >
+      <DamhwaDeliveryContainer />
+    </Layout>
+  );
+};
+
+export default DamhwaDelivery;
