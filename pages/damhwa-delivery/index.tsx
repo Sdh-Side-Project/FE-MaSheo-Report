1f816aca41db0d43e19b68ded2bbee217eba04fd
Namkyung <srirachacho@gmail.com>
2022-12-11 03:24:14 +0900

    :white_check_mark: Update: 맛, 담화배송, 리뷰 페이지 푸터 메시지 추가 및 스타일 일부 수정

--- pages/damhwa-delivery/index.tsx
+++ pages/damhwa-delivery/index.tsx
@@ -1,21 +1,8 @@
 import React from 'react';
 import DamhwaDeliveryContainer from '../../components/DamhwaDelivery/DamhwaDeliveryComponent';
-import Layout from '../../components/layout';
 
 const DamhwaDelivery = () => {
-  return (
-    <Layout
-      user={'개발팀'}
-      firstLine={'올해 이용한'}
-      keyword={'담화배송 횟수'}
-      closingLine={'는?'}
-      footerMessage={`내년에도 담화배송을 잘 부탁해요!`}
-      footerImgUrl={'/images/layout/hyeon.png'}
-      footerImgName="현겸"
-    >
-      <DamhwaDeliveryContainer />
-    </Layout>
-  );
+  return <DamhwaDeliveryContainer />;
 };
 
 export default DamhwaDelivery;
--- pages/review/index.tsx
+++ pages/review/index.tsx
@@ -15,9 +15,30 @@ const Review = () => {
   const [showReview, setShowReview] = useState(false);
   const [reviewData, setReviewData] = useState(review);
 
+  const [footerMessage, setFooterMessage] = useState('올 한해 나는 얼만큼 리뷰를 남겼을까요?');
+
   useEffect(() => {
     setShowReview(true);
+    setTimeout(() => {
+      getFooterMessage();
+    }, 3000);
   }, []);
+
+  const getFooterMessage = () => {
+    if (30 > reviewData.rate && reviewData.rate >= 0) {
+      return setFooterMessage('2023년 새해, 술담화에 리뷰 꼬옥 남겨주기 약속! 🤙');
+    }
+    if (60 > reviewData.rate && reviewData.rate >= 30) {
+      return setFooterMessage(
+        '세심하고 진정성 있는 리뷰를 꼼꼼히 남기는 당신! 내년에는 술담화의 리뷰왕이 되어주세요 🤝'
+      );
+    }
+    if (100 >= reviewData.rate && reviewData.rate >= 60) {
+      return setFooterMessage(
+        '술담화의 발전에 큰 힘이 되는 당신께 감사합니다 💕 우리 내년에도 이대로 쭉 함께해요!'
+      );
+    }
+  };
 
   if (!showReview) return null;
 
@@ -27,7 +48,7 @@ const Review = () => {
       firstLine={'주문 대비'}
       keyword={'작성한 리뷰 비율'}
       closingLine={'은?'}
-      footerMessage={``}
+      footerMessage={footerMessage}
       footerImgUrl={'/images/layout/namkyung.png'}
       footerImgName="남경"
     >
