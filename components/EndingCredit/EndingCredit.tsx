597d2c29c4a9451c93009755db7416ded4fe837e
ByeongSooLee-KR <djaxornwkd12@gmail.com>
2022-12-29 12:43:05 +0900

    엔딩크레딧 최종 수정

--- components/EndingCredit/EndingCredit.tsx
+++ components/EndingCredit/EndingCredit.tsx
@@ -38,21 +38,17 @@ const EndingCreditContainer = () => {
             </div>
             <div className="leading-loose">
               <p className="mb-10">개발팀이 준비한 선물 어떠셨나요?</p>
-              <p className="mb-10">쟤네들 퇴근 후에 매일 모여서 무슨 얘기를 하나 궁금하셨죠?</p>
+              <p className="mb-10">쟤네들 퇴근 후에 매일 모여서 무슨 얘기를 하나 궁금하셨죠?🤫</p>
+              <p className="mb-10">껄껄 이거 만들려고 두 달 반 전부터 준비했어요😀</p>
+              <p className="mb-10">물론 기획, 디자이너 없이 저희끼리 만들어서 조금 엉성해요😭</p>
               <p className="mb-10">
-                바로 이걸 보여주기 위해 약 두 달 반의 시간동안 매 주 두 번씩 모여서 조금씩 만들어
-                보았어요!
+                하지만 어려워진 상황 속 여러분들에게 작은 서프라이즈를 만들고 싶은 생각으로 뭉쳐
               </p>
-              <p className="mb-10">물론 기획, 디자이너 없이 저희끼리 만들어서 많이 엉성해요.</p>
+              <p className="mb-10">퇴근 후에 조금씩 조금씩 만든 결과물이에요🙌!</p>
               <p className="mb-10">
-                하지만 하반기부터 힘들어진 상황 속 여러 분들에게 작은 서프라이즈를 만들고 싶은
-                생각으로
+                재밌게 봐주셨으면 좋겠고 다가오는 23년도 우리 다같이 화이팅 해보아요🤗!
               </p>
-              <p className="mb-10">개발팀이 뭉쳐 퇴근 후에 조금씩 조금씩 만든 결과물이에요!</p>
-              <p className="mb-10">
-                재밌게 봐주셨으면 좋겠고 다가오는 23년도 우리 다같이 화이팅 해보아요!
-              </p>
-              <p className="mb-20">사랑해요 술담화!</p>
+              <p className="mb-20">사랑해요 술담화🥰</p>
               <p className="text-center text-9xl">-끝-</p>
             </div>
           </div>
